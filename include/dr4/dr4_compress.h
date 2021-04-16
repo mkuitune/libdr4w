e0673f4d67282ae7e64869bda3b6a4e1da726bb2
Mikko Kuitunen <mikko.kuitunen@iki.fi>
2021-04-16 11:18:49 +0300

    generic vector POD compress/uncompress

--- include/dr4/dr4_compress.h
+++ include/dr4/dr4_compress.h
@@ -11,4 +11,29 @@ namespace dr4 {
 	void Compress(const std::string& uncompressed,	std::string& compressed);
 	void Compress(const char* source,size_t srcLen,	std::string& compresssed);
 
+	//
+	// Generic compressor api for POD data stored in std::vector
+	//
+	template<class VEC>
+	bool UncompressGeneric(const std::string& compressed, VEC& out){
+			size_t len;
+			bool succ = UncompressLen(compressed, len);
+			if (!succ) {
+				return false;
+			}
+
+			size_t nelem = len / sizeof(typename VEC::value_type);
+			out = VEC(nelem);
+			succ = UncompressBytes(compressed, (char*)&out[0]);
+			if (!succ) {
+				return false;
+			}
+	}
+	template<class VEC>
+	void CompressGeneric(const VEC& vec, std::string& compressed){
+		const char* data = (const char*)&vec[0];
+		size_t bytecount = vec.size() * sizeof(typename VEC::value_type);
+		Compress(data, bytecount, compressed);
+	}
+
 }
--- include/dr4/dr4_compressible_trimesh.h
+++ include/dr4/dr4_compressible_trimesh.h
@@ -15,26 +15,14 @@ namespace dr4 {
 		// Compression API
 		//
 		void compress(std::string& out) const {
-			// size, bytes
-			const char* data = (const char*)&vertices[0];
-			size_t bytecount = vertices.size() * sizeof(Tripletf);
-			Compress(data, bytecount, out);
+			CompressGeneric(vertices, out);
 		}
 
 		static CompressibleTrimesh Uncompress(const std::string& buffer) {
-			size_t len;
-			bool succ = UncompressLen(buffer, len);
-			if (!succ) {
-				// TODO BETTER ERROR	
-				return {};
-			}
-
-			size_t nvertices = len / sizeof(Tripletf);
 			CompressibleTrimesh mesh;
-			mesh.vertices = std::vector<Tripletf>(nvertices);
-			succ = UncompressBytes(buffer, (char*)&mesh.vertices[0]);
+			bool succ = UncompressGeneric(buffer, mesh.vertices);
 			if (!succ) {
-				// TODO BETTER ERROR	
+				// TODO BETTER ERROR handling
 				return {};
 			}
 			return mesh;
